d99ae8ce6a2fc77f7d95b08cbc8d670db7471ed1
arimvasa <126396105+arimvasa@users.noreply.github.com>
2023-05-14 23:22:00 +0300

    Add html for logic

--- a.js
+++ a.js
@@ -1,23 +1,27 @@
+const ROCK = 'rock'
+const PAPER = 'paper'
+const SCISSORS = 'scissors'
 
 // Returns a random choice between rock, paper and scissors
-function getComputerChoice() {
-    let computerChoices = ["paper", "rock", "scissors"]
+const getComputerChoice = () => {
+    let computerChoices = [PAPER, ROCK, SCISSORS]
     // Returns 0,1 or 2
     let randomIndex = Math.floor(Math.random() * 3)
     return computerChoices[randomIndex]
 }
 
-function playRound(playerSelection, computerSelection) {
-
-    playerSelection = playerSelection.toLowerCase();
+const playRound = (playerSelection, computerSelection) => {
     let result
+    if (![ROCK, PAPER, SCISSORS].includes(playerSelection)) {
+        throw new Error('Player selection is not accepted')
+    }
 
     if ((playerSelection === computerSelection)) {
         result === "tie"
     }
-    else if ((playerSelection === "rock" && computerSelection === "paper")
-        || (playerSelection === "paper" && computerSelection === "scissors")
-        || (playerSelection === "scissors" && computerSelection === "rock")) {
+    else if ((playerSelection === ROCK && computerSelection === PAPER)
+        || (playerSelection === PAPER && computerSelection === SCISSORS)
+        || (playerSelection === SCISSORS && computerSelection === ROCK)) {
         result = `computer`
     }
     else {
@@ -27,46 +31,43 @@ function playRound(playerSelection, computerSelection) {
     return result
 }
 
-
-function game() {
+const game = () => {
 
     let computerScore = 0;
     let playerScore = 0;
     let playerSelection;
     let computerSelection;
 
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}`)
 
-        playerSelection = window.prompt("what do you want?")
-        computerSelection = getComputerChoice()
-        const result = playRound(playerSelection, computerSelection)
 
-        // auksanei to score ston kathe paixti
-        if (result === "player") {
-            playerScore++;
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-        }
-        else if (result === "computer") {
-            computerScore++;
-            console.log(`You Lose! ${playerSelection} loses to ${computerSelection}`)
-        }
-        else {
-            console.log(`It's a tie!!!!`)
+    computerSelection = getComputerChoice()
+    const result = playRound(playerSelection, computerSelection)
 
-        }
-
+    // auksanei to score ston kathe paixti
+    if (result === "player") {
+        playerScore++;
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
     }
-    console.log(`Playerscore:${playerScore}`)
-    console.log(`Computerscore:${computerScore}`)
-
-    if (playerScore > computerScore) { console.log("You win the game!"); }
-
-    else if (playerScore < computerScore) { console.log("You lose the game!") }
-
-    else { console.log("It's a tie game!") }
-
+    else if (result === "computer") {
+        computerScore++;
+        console.log(`You Lose! ${playerSelection} loses to ${computerSelection}`)
+    }
+    else {
+        console.log(`It's a tie!!!!`)
+    }
 }
 
-// start
-game()
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector('#paperButton')
+const scissorsButton = document.querySelector('#scissorsButton')
+
+rockButton.addEventListener('click', () => {
+    playRound(ROCK, getComputerChoice());
+});
+paperButton.addEventListener('click', () => {
+    playRound(PAPER, getComputerChoice());
+});
+scissorsButton.addEventListener('click', () => {
+    playRound(SCISSORS, getComputerChoice())
+});
+
